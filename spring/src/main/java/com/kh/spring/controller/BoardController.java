5a53cb0b4fb89deb940db7c123bd320c4265b379
paiktmddnjs <paiktmddnjs@naver.com>
2025-10-22 17:50:18 +0900

    no message

--- spring/src/main/java/com/kh/spring/controller/BoardController.java
+++ spring/src/main/java/com/kh/spring/controller/BoardController.java
@@ -1,18 +1,15 @@
 package com.kh.spring.controller;
 
-import ch.qos.logback.core.model.Model;
+
 import com.kh.spring.model.vo.Board;
 import com.kh.spring.model.vo.PageInfo;
 import com.kh.spring.service.BoardService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,9 +29,7 @@ public class BoardController {
     }
 
         @PostMapping("/list.bo")
-        public String selectAllBoard(@RequestParam(value = "cpage", required = false) Integer cpage,
-                                     HttpServletRequest request,
-                                     HttpServletResponse response) {
+        public String selectAllBoard(@RequestParam(value = "cpage", required = false) Integer cpage , Model model) {
             int currentPage = (cpage != null) ? cpage : 1; // 요청한 페이지 또는 기본값
             int listCount = boardService.selectAllBoardCount();
 
@@ -45,8 +40,8 @@ public class BoardController {
 
             ArrayList<Board> list = boardService.selectAllBoard(pi);
 
-            request.setAttribute("list", list);
-            request.setAttribute("pi", pi);
+            model.addAttribute("list", list);
+            model.addAttribute("pi", pi);
 
             return "/WEB-INF/views/board/listView"; // 포워드할 뷰 경로
         }
